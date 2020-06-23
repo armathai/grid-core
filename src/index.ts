df72322ca15746be6eb6f834b4050ec9428807ed
Ashot Avagyan <avagyanah@gmail.com>
2020-06-23 15:40:12 +0400

    exporting geom classes Rect/Point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Cell } from './Cell';
 import { CellAlign, CellScale, ICellConfig, IDebug, IDimension } from './Types';
+import { Point } from './utils/geom/Point';
+import { Rect } from './utils/geom/Rect';
 import { align, fit } from './utils/Utils';
 
-export { Cell, CellAlign, CellScale, IDimension, align, fit, ICellConfig, IDebug };
+export { Cell, CellAlign, CellScale, IDimension, align, fit, ICellConfig, IDebug, Rect, Point };
